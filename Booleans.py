d06507eed963e194c8e695dd8517552602fe496e
Arni <arnurrrrs@gmail.com>
2025-01-28 12:24:35 +0500

    Lab2
    Signed-off-by: Arni <arnurrrrs@gmail.com>

---
+++ Booleans.py
@@ -0,0 +1,37 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+  x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
+#The following will return False:
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+'''One more value, or object in this case, evaluates to False, 
+and that is if you have an object that is made from a class 
+with a __len__ function that returns 0 or False:'''
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
